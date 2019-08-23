091067f537ebd34e3e2aee18ee036bfb70e9536e
Kenya <kenya55@msn.com>
2019-08-23 10:21:58 -0400

    added function for loading gifs

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -30,3 +30,31 @@ function adding_keywords() {
     var new_button = '<button class="btn btn-primary btn-sm" onclick="get_data(\'' + new_keyword + '\')">' + new_keyword + '</button>';
     $('.content_buttons').append(new_button);
 }
+
+function load_gifs() {
+    if (gif_data.length === 0) return;
+    $('#gif_images').html('');
+    var index = 0;
+    var row_html = '<div class="row">';
+    for (var i = 0; i < gif_data.length; i++) {
+        index++;
+        if (index === 3) {
+            row_html += '<div class="col-md-4"><p>Rating: ' + gif_data[i].rating.toUpperCase() + '</p>';
+            row_html += '<img src="' + gif_data[i].images.fixed_height_still.url + '" data-static="' + gif_data[i].images.fixed_height_still.url
+                + '" data-gif="' + gif_data[i].images.fixed_height.url + '" data-id="1" onclick="change_gif(this)"/>';
+            row_html += '</div>';
+            $('#gif_images').append(row_html);
+            index = 0;
+            row_html = '<div class="row">';
+        } else {
+            row_html += '<div class="col-md-4"><p>Rating: ' + gif_data[i].rating.toUpperCase() + '</p>';
+            row_html += '<img src="' + gif_data[i].images.fixed_height_still.url + '" data-static="' + gif_data[i].images.fixed_height_still.url
+                + '" data-gif="' + gif_data[i].images.fixed_height.url + '" data-id="1" onclick="change_gif(this)"/>';
+            row_html += '</div>';
+        }
+    }
+    if (gif_data.length % 3 !== 0) {
+        row_html += '</div>';
+        $('#gif_images').append(row_html);
+    }
+}
