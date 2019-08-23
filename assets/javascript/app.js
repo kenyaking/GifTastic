c55f6522c2bb9b0c1e19c6f25404b227831b2f05
Kenya <kenya55@msn.com>
2019-08-23 10:12:07 -0400

    added function for keywords in app.js file

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -11,3 +11,22 @@ var keywords = [
     'dancer'
 
 ];
+var gif_data = [];
+$(document).ready(function () {
+    
+    var html_buttons = '';
+    for (var i = 0; i < keywords.length; i++) {
+        html_buttons += '<button class="btn btn-primary btn-sm" onclick="get_data(\'' + keywords[i] + '\')">' + keywords[i] + '</button>';
+    }
+    $('.content_buttons').html(html_buttons);
+
+});
+
+function adding_keywords() {
+    var new_keyword = $('#adding_keyword').val();
+    if (new_keyword === '') return;
+    if (keywords.indexOf(new_keyword) > -1) return;
+    keywords.push(new_keyword);
+    var new_button = '<button class="btn btn-primary btn-sm" onclick="get_data(\'' + new_keyword + '\')">' + new_keyword + '</button>';
+    $('.content_buttons').append(new_button);
+}
